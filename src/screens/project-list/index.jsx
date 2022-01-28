d2c7bfda6ac77039eefff48f76a857f4ee7c9844
EFSZ\zhouming.wang <zhouming.wang@ecoflow.com>
2022-01-28 15:35:57 +0800

    feat: 使用useDebounce，区分hook和普通函数

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,31 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
-import qs from 'qs';
-import { cleanObject } from "../../utils";
+import qs from "qs";
+import { cleanObject, useDebounce, useMount } from "../../utils";
 
 const api_url = process.env.REACT_APP_API_URL;
 const ProjectList = () => {
   const [params, setParams] = useState({ name: "", personId: "" });
   const [users, setUsers] = useState([]);
   const [projects, setProjects] = useState([]);
+  const debounceParams = useDebounce(params, 500);
+
   useEffect(() => {
     fetch(
-      `${api_url}/projects?${qs.stringify(cleanObject(params))}`
+      `${api_url}/projects?${qs.stringify(cleanObject(debounceParams))}`
     ).then(async (response) => {
       if (response.ok) {
         setProjects(await response.json());
       }
     });
-  }, [params]);
+  }, [debounceParams]);
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${api_url}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
     });
-  }, []);
+  });
 
   return (
     <>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,10 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) =>
   value === 0 || value === false ? true : !!value;
 
+///
+/// 什么时候用函数，什么时候用hook呢？简单区分就是函数中需要使用别的hook（useState等）
 export const cleanObject = (object) => {
   const result = { ...object };
   Object.keys(object).forEach((key) => {
@@ -10,4 +14,24 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+// 在这里使用Hook的原因
+// value和debounceValue之间有个转换，这个转换需要借助react状态（响应式的，值改变的时候页面改变或做一些操作）
+export const useDebounce = (value, delay = 500) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebounceValue(value), delay);
+    /* 在上一个useEffect处理完后运行，因此只会留下最后一个定时器 */
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debounceValue;
 };
